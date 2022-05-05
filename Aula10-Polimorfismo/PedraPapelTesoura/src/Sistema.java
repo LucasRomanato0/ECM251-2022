7c67c536167d8a446d84b9c11ecff0bac711d6ee
LUCAS ROMANATO DE OLIVEIRA <20.00313-7@maua.br>
2022-05-05 12:33:48 -0300

    polimorfismo

--- Aula10-Polimorfismo/PedraPapelTesoura/src/Sistema.java
+++ Aula10-Polimorfismo/PedraPapelTesoura/src/Sistema.java
@@ -3,28 +3,33 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Sistema {
     public static void rodar(){
         //Usuario escolhe a jogada
-        Jogada jogada1 = new Tesoura();
+        Jogada jogada1 = new Spoke();
         //Sorteio da jogada para o PC
         Jogada jogada2 = sortearJogada();
         //Avaliação das jogadas
         String resultado = avaliaJogadas(jogada1, jogada2);
         //Exibição do resultado
+        System.out.println("Primeira jogada: \n" + jogada1);
+        System.out.println("   VERSUS   ");
+        System.out.println("Segunda jogada: \n" + jogada2);
         System.out.println("Resultado:" + resultado);
     }
 
     private static Jogada sortearJogada() {
-        Jogada jogadas[] = new Jogada[3];
+        Jogada jogadas[] = new Jogada[5];
         jogadas[0] = new Pedra();
         jogadas[1] = new Papel();
         jogadas[2] = new Tesoura();
+        jogadas[3] = new Lagarto();
+        jogadas[4] = new Spoke();
         return jogadas[ThreadLocalRandom.current().nextInt(jogadas.length)];
     }
 
     private static String avaliaJogadas(Jogada jogada1, Jogada jogada2) {
         if(jogada1.verificarVenceu(jogada2))
-            return "Jogada 1";
+            return " Jogada 1";
         if(jogada2.verificarVenceu(jogada1))
-            return "Jogada 2";
-        return "Empate";
+            return " Jogada 2";
+        return " Empate";
     }
 }
