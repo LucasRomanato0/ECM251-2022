6c67340b2c37ee7d0b27fdab828008e979aa9e98
LUCAS ROMANATO DE OLIVEIRA <20.00313-7@maua.br>
2022-08-18 12:41:46 -0300

    Criação da classe Conta

---
+++ Aula15-lab/modelos/usuarios/contas.py
@@ -0,0 +1,8 @@
+class Conta:
+    def __init__(self, id=None, historico_de_compras = []) -> None:
+        self._id = id
+        self._historico_de_compras = historico_de_compras
+
+    def get_historico_de_compras(self):
+        return self._historico_de_compras
+        
