a440c02d573d0c849ed7a4434a48bde8729bd6e3
LUCAS ROMANATO DE OLIVEIRA <20.00313-7@maua.br>
2022-08-25 12:38:21 -0300

    Aula 18

--- Aula18/manipulacao_arquivos/main.py
+++ Aula18/manipulacao_arquivos/main.py
@@ -1,15 +1,25 @@
 #Programa que escreve em um arquivo
+try:
+    arquivo = open("data/dados.txt", "a")
+    continuar = True
 
-arquivo = open("dados.txt", "a")
-continuar = True
+    while continuar:
+        #time é str
+        time = input("Time (vazio para sair): ")
+        #toda str vazia é falsa
+        #entra no if se a str está vazia
+        if not time:
+            continuar = False
+            continue
+        arquivo.write(time+'\n')
+    arquivo.close()
 
-while continuar:
-    #time é str
-    time = input("Time (vazio para sair): ")
-    #toda str vazia é falsa
-    #entra no if se a str está vazia
-    if not time:
-        continuar = False
-        continue
-    arquivo.write(time+'\n')
-arquivo.close()
+    x = 9+"ola"
+except FileNotFoundError:
+    print('Arquivo ou diretório não encontrado')
+except ZeroDivisionError:
+    print('Alguém tentou dividir por zero')
+except:
+    print('Algo de errado aconteceu!')
+finally:
+    print('Enfim terminou!')
