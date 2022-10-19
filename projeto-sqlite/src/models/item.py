fb48c80b0c75757404609fdd345d925f459a8239
LucasRomanato0 <20.00313-7@maua.br>
2022-10-19 11:12:06 -0300

    database sql

---
+++ projeto-sqlite/src/models/item.py
@@ -0,0 +1,11 @@
+from turtle import pen
+
+
+class Item:
+       def __init__(self, id, nome, preco) -> None:
+              self.id = id
+              self.nome = nome
+              self.preco = preco
+       
+       def __str__(self) -> str:
+              return f'Item[ "id": {self.id} - "nome": {self.nome} - "preço": {self.preco} ]'
